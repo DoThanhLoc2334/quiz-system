2b12a93b48d9791b63477a8ca0b016c3e4216dd4
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2026-04-03 23:42:51 +0700

    fix

--- GK Quiz in C -Codewithc.c
+++ GK Quiz in C -Codewithc.c
@@ -1,248 +1,115 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-//CODEWITHC.COM
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    const char *question;
+    const char *options;
+    char correct_answer;
+} QuizQuestion;
+
+static void clear_screen(void)
 {
-    int x = 0;
-    char ch1[100],ch2,ch3,ch4,ch5,ch6,ch7,ch8,ch9,ch10,ch11,ch12,ch13;
-    printf("Enter Your Name: ");
-    scanf("%[^\n]", ch1);
-    getchar();
     system("cls");
-    printf("Welcome %s\n", ch1);
-    printf("What is called as ' THE HOLY LAND'?\n");
-    printf("a.Jerusalem\nb.Mathura\nc.Mecca");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch2);
-    getchar();
-    if(ch2=='a')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE ROOF OF THE WORLD'?\n");
-    printf("a.Nepal\nb.Rome\nc.Tibet");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch3);
-    getchar();
-    if(ch3=='c')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE LAND OF RISING SUN'?\n");
-    printf("a.Chicago\nb.Japan\nc.Tibet");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch4);
-    getchar();
-    if(ch4=='b')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE GIFT OF NILE'?\n");
-    printf("a.Chicago\nb.Egypt\nc.Africa");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch5);
-    getchar();
-    if(ch5=='b')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE LAND OF MIDNIGHT SUN'?\n");
-    printf("a.Norway\nb.Japan\nc.Australia");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch6);
-    getchar();
-    if(ch6=='a')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE LAND OF THUNDERBOLT'?\n");
-    printf("a.Bhutan\nb.Canada\nc.Arab");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch7);
-    getchar();
-    if(ch7=='a')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE WINDY CITY?\n");
-    printf("a.Jerusalem\nb.Japan\nc.Chicago");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch8);
-    getchar();
-    if(ch8=='c')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE LAND OF WHITE ELEPHANTS'?\n");
-    printf("a.Bangladesh\nb.Thailand\nc.India");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch9);
-    getchar();
-    if(ch9=='b')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d\n", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-    system("cls");
-    printf("What is called as ' THE CITY OF SEVEN HILLS'?\n");
-    printf("a.Rome\nb.Nilgiri Hills\nc.Tibet");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch10);
-    getchar();
-    if(ch10=='a')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
-    }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
-    }
-
-
-    system("cls");
-printf("What is called as ' THE LAND OF FIRE AND ICE'?\n");
-printf("a.Iceland\nb.Greenland\nc.Finland");
-printf("\nEnter your answer: ");
-scanf("%c", &ch12);
-getchar();
-if(ch12=='a')
-{
-    x = x + 10;
-    printf("Good Job. Your score is %d", x);
-    getchar();
-}
-else
-{
-    printf("Sorry wrong answer.");
-    getchar();
 }
 
-system("cls");
-printf("What is called as ' THE PEARL OF THE ORIENT'?\n");
-printf("a.Vietnam\nb.Philippines\nc.Thailand");
-printf("\nEnter your answer: ");
-scanf("%c", &ch13);
-getchar();
-if(ch13=='b')
+static void wait_for_enter(void)
 {
-    x = x + 10;
-    printf("Good Job. Your score is %d", x);
-    getchar();
+    printf("\nPress Enter to continue...");
+    while (getchar() != '\n') {
+    }
 }
-else
+
+static int ask_question(const QuizQuestion *q, int current_score)
 {
-    printf("Sorry wrong answer.");
-    getchar();
+    char answer;
+
+    clear_screen();
+    printf("%s\n", q->question);
+    printf("%s\n", q->options);
+    printf("Enter your answer: ");
+
+    if (scanf(" %c", &answer) != 1) {
+        return current_score;
+    }
+
+    while (getchar() != '\n') {
+    }
+
+    if (answer == q->correct_answer || answer == (q->correct_answer - 32)) {
+        current_score += 10;
+        printf("Good job. Your score is %d\n", current_score);
+    } else {
+        printf("Sorry, wrong answer. Your score is %d\n", current_score);
+    }
+
+    wait_for_enter();
+    return current_score;
 }
-    
-    system("cls");
-    printf("What is called as ' THE DARK CONTIENENT'?\n");
-    printf("a.Asia\nb.Australia\nc.Africa");
-    printf("\nEnter your answer: ");
-    scanf("%c", &ch11);
-    getchar();
-    if(ch11=='c')
-    {
-        x = x + 10;
-        printf("Good Job. Your score is %d", x);
-        getchar();
+
+int main(void)
+{
+    char name[100];
+    int score = 0;
+    int total_questions;
+    int max_score;
+    int percentage;
+    int i;
+
+    const QuizQuestion questions[] = {
+        {"What is called as 'THE HOLY LAND'?", "a. Jerusalem\nb. Mathura\nc. Mecca", 'a'},
+        {"What is called as 'THE ROOF OF THE WORLD'?", "a. Nepal\nb. Rome\nc. Tibet", 'c'},
+        {"What is called as 'THE LAND OF RISING SUN'?", "a. Chicago\nb. Japan\nc. Tibet", 'b'},
+        {"What is called as 'THE GIFT OF NILE'?", "a. Chicago\nb. Egypt\nc. Africa", 'b'},
+        {"What is called as 'THE LAND OF MIDNIGHT SUN'?", "a. Norway\nb. Japan\nc. Australia", 'a'},
+        {"What is called as 'THE LAND OF THUNDERBOLT'?", "a. Bhutan\nb. Canada\nc. Arab", 'a'},
+        {"What is called as 'THE WINDY CITY'?", "a. Jerusalem\nb. Japan\nc. Chicago", 'c'},
+        {"What is called as 'THE LAND OF WHITE ELEPHANTS'?", "a. Bangladesh\nb. Thailand\nc. India", 'b'},
+        {"What is called as 'THE CITY OF SEVEN HILLS'?", "a. Rome\nb. Nilgiri Hills\nc. Tibet", 'a'},
+        {"What is called as 'THE LAND OF FIRE AND ICE'?", "a. Iceland\nb. Greenland\nc. Finland", 'a'},
+        {"What is called as 'THE PEARL OF THE ORIENT'?", "a. Vietnam\nb. Philippines\nc. Thailand", 'b'},
+        {"What is called as 'THE DARK CONTINENT'?", "a. Asia\nb. Australia\nc. Africa", 'c'}
+    };
+
+    total_questions = (int)(sizeof(questions) / sizeof(questions[0]));
+    max_score = total_questions * 10;
+
+    printf("Enter your name: ");
+    if (fgets(name, sizeof(name), stdin) == NULL) {
+        return 1;
     }
-    else
-    {
-        printf("Sorry wrong answer.");
-        getchar();
+
+    for (i = 0; name[i] != '\0'; i++) {
+        if (name[i] == '\n') {
+            name[i] = '\0';
+            break;
+        }
     }
-    system("cls");
-    if(x == 100) {
-        printf("No cheating...... You have done this earlier also.");
+
+    clear_screen();
+    printf("Welcome %s\n", name);
+    wait_for_enter();
+
+    for (i = 0; i < total_questions; i++) {
+        score = ask_question(&questions[i], score);
     }
-    if(x == 90){
-        printf("You are extremely intelligent. Your Score is 90");
-    }
-    if(x == 80){
-        printf("You are intelligent. Your Score is 80");
-    }
-    if(50 == x || x == 70 || x == 60) {
-        printf("You are average. Your Score is %d. Better luck next time\n", x);
-    }
-    else if(x <= 40){
-        printf("No use........ Not even 5 questions right");
+
+    clear_screen();
+    percentage = (score * 100) / max_score;
+
+    printf("Quiz finished.\n");
+    printf("Final score: %d/%d\n", score, max_score);
+
+    if (percentage == 100) {
+        printf("Perfect score. Excellent work!\n");
+    } else if (percentage >= 80) {
+        printf("You are extremely intelligent.\n");
+    } else if (percentage >= 60) {
+        printf("You are intelligent.\n");
+    } else if (percentage >= 40) {
+        printf("You are average. Better luck next time.\n");
+    } else {
+        printf("Keep practicing. You can improve a lot.\n");
     }
 
     return 0;
-
 }
